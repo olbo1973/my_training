a833bbabb71fe637980cded8ad8868b158b5532d
olbo1973 <o.boldyreff@gmail.com>
2024-09-09 12:35:42 +0300

    Add files via upload

---
+++ training_task2.py
@@ -0,0 +1,5 @@
+completed_tasks=12
+hours_spent=1.5
+course_name='Python'
+Time_for_one_task=hours_spent/completed_tasks
+print('Курс:',course_name,",всего задач:",completed_tasks,",затрачено часов:",hours_spent,",среднее время выполнения",Time_for_one_task)

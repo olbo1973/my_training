7b64a75e0dfd889fa3cfadd104b629e1e2d0c6b1
olbo1973 <o.boldyreff@gmail.com>
2024-09-10 12:15:39 +0300

    10.09/f

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+my_string=input('введите текст: ')
+
+print('введено символов: ',len(my_string))#кол-во символов
+
+print(my_string.upper())#печать заглавными
+
+print(my_string.upper().lower())#преобразовать в строчные
+
+print(my_string.replace( ' ' ,''))#убрать пробелы
+
+print(my_string[0])#печать первой буквы
+
+print(my_string[-1])# печать последней

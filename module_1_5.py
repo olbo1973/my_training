2579d247a79c09c0fe1aa19df1d4af5529a454b4
olbo1973 <o.boldyreff@gmail.com>
2024-09-11 12:16:55 +0300

    11.09/1.2

--- module_1_5.py
+++ module_1_5.py
@@ -2,3 +2,13 @@
 immutable_var=tuple_="abc",123,[456]
 
 print(immutable_var)
+#tuple_[1]=5
+# TypeError: 'tuple' object does not support item assignment
+#print(tuple_)
+
+mutable_list=['zyw',678,True]
+print(mutable_list)
+#index = mutable_list.index(True)
+#print(index)
+mutable_list[2]=False
+print(mutable_list)

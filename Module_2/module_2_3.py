1fa7a7abb592d7364b0d6ab53d629649d89b3301
olbo1973 <o.boldyreff@gmail.com>
2024-09-16 19:06:49 +0300

    16.09/2

--- Module_2/module_2_3.py
+++ Module_2/module_2_3.py
@@ -1,1 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+print('if a<=0, stop.')
+value = 0
+while len(my_list) > value:
+    if my_list[value] > 0:
+        print(my_list[value])
+    if my_list[value] < 0:
+        break
+    value += 1
+
+

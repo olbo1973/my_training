2cace415051a6c975fcd073ae10bd194e7adbb47
olbo1973 <o.boldyreff@gmail.com>
2024-09-24 10:39:01 +0300

    24.09/2

--- Module_2/module_2_5.py
+++ Module_2/module_2_5.py
@@ -1,1 +1,17 @@
-def get_matrix ():
+def get_matrix(n, m, value):
+    matrix = []
+    # цикл for для кол-ва строк матрицы, n повторов
+    for i in range(n):
+        matrix.append([])
+        # цикл for для кол-ва столбцов матрицы, m повторов
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)

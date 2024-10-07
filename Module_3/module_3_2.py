adeb3b3059d19c8c1a6ea27ddd5b2d0ee1ac77e3
olbo1973 <o.boldyreff@gmail.com>
2024-10-07 15:00:54 +0300

    6.10/2

--- Module_3/module_3_2.py
+++ Module_3/module_3_2.py
@@ -1,16 +1,18 @@
-def send_email(message, recipient, *, sender = 'university.help@gmail.com'):
-    mail_recipient = '@' in recipient and (recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'))
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    mail_recipient = '@' in recipient and (
+                recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'))
     mail_sender = '@' in sender and (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'))
     if mail_recipient and mail_sender:
-        if recipient == sender:
-            print('Нельзя отправить письмо самому себе!')
-        else:
+        if recipient != sender:
             if sender == 'university.help@gmail.com':
                 print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
             else:
                 print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+        else:
+            print('Нельзя отправить письмо самому себе!')
     else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')

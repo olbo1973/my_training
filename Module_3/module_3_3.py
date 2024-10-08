f8d11db444d2773480b7737ddbd2749b6bc252f6
olbo1973 <o.boldyreff@gmail.com>
2024-10-08 12:56:41 +0300

    7.10/2

--- Module_3/module_3_3.py
+++ Module_3/module_3_3.py
@@ -1,16 +1,17 @@
 # Функция с параметрами по умолчанию:
-def print_params(a=1, b='строка', c=True,):
+def print_params(a=1, b='строка', c=True, ):
     print(a, b, c)
     print(a, b)
     print(c)
+
+
 print_params()
 print_params(b=25)
 print_params(c=[1, 2, 3])
 
-
 # Распаковка параметров:
 values_list = ['Vasya', 999, 5.6]
-values_dict = {'a': [1, 2, 3], 'b': 345, 'c':'qwerty' }
+values_dict = {'a': [1, 2, 3], 'b': 345, 'c': 'qwerty'}
 print_params(*values_list)
 print_params(**values_dict)
 # Распаковка + отдельные параметры:

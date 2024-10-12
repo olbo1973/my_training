6c3e9331cea7bf95c1e25b5e143e0e256cf4bce0
olbo1973 <o.boldyreff@gmail.com>
2024-10-12 13:38:14 +0300

    12.10/1

--- module_1_5.py
+++ module_1_5.py
@@ -1,16 +1,15 @@
-
-immutable_var=tuple_="abc",123,[456]
+immutable_var = tuple_ = "abc", 123, [456]
 
 print(immutable_var)
-#tuple_[1]=5
+# tuple_[1]=5
 # TypeError: 'tuple' object does not support item assignment
-#print(tuple_)
+# print(tuple_)
 
-mutable_list=['zyw',678,True]
+mutable_list = ['zyw', 678, True]
 print(mutable_list)
-#index = mutable_list.index(True)
-#print(index)
-mutable_list[2]=False
-mutable_list[1]='6,7,8'
-mutable_list[0]='ZYW'
+# index = mutable_list.index(True)
+# print(index)
+mutable_list[2] = False
+mutable_list[1] = '6,7,8'
+mutable_list[0] = 'ZYW'
 print(mutable_list)
--- module_1_6.py
+++ module_1_6.py
@@ -1,19 +1,19 @@
-my_dict={'a':1,'b':2,'c':3}
+my_dict = {'a': 1, 'b': 2, 'c': 3}
 print(my_dict)
-print('выбрано: ',my_dict['b'])#Existing value
+print('выбрано: ', my_dict['b'])  # Existing value
 
-print(my_dict.get('d','Не найден'))#Not existing value
-my_dict.update(d = 4,e = 5)
-print('новый:',my_dict)
-a=my_dict.pop('c')
-print('извлечено:',a)
+print(my_dict.get('d', 'Не найден'))  # Not existing value
+my_dict.update(d=4, e=5)
+print('новый:', my_dict)
+a = my_dict.pop('c')
+print('извлечено:', a)
 print(my_dict)
 
-my_set ={'a','b','c',1,2,3,4,5,3,4,5}
-print(my_set )
-d=('z',58,2)
-my_set .update(d)
-print(my_set )
+my_set = {'a', 'b', 'c', 1, 2, 3, 4, 5, 3, 4, 5}
+print(my_set)
+d = ('z', 58, 2)
+my_set.update(d)
+print(my_set)
 print(my_set.discard(2))
 print(my_set.remove(4))
-print(my_set )
+print(my_set)

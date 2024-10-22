d37d5c0a8c8cedb1a18fac07206687accdd0e51e
olbo1973 <o.boldyreff@gmail.com>
2024-10-22 21:05:38 +0300

    22.10/1

---
+++ module_4/fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second != 0:
+        x = first / second
+        return x
+    else:
+        return 'Ошибка'
---
+++ module_4/true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(first, second):
+    if second != 0:
+        x = first / second
+        return x
+    else:
+        return inf
